9ee6f06c66b2c7cf01f0bd8a07a133d2cbd6dcd5
rencunzhi <rencunzhi@palm.science>
2016-10-10 21:07:41 -0700

    partily realize

--- EE562/HW1.py
+++ EE562/HW1.py
@@ -1,15 +1,8 @@
 """
-Count how many states are searched to find each solution; there are four.
-For your counting keep track of 3 kinds of states you generate:
-illegal states in which the cannibals eat the missionaries,
-repeated states that are the same as an ancestor state on the same path,
-total states searched (ie. all states except the illegal and repeated ones).
-
-the solutions your program finds, each solution being an ordered
-list of states from initial state to goal state. Format as follows:
-(3,3,L)(3,1,R)(3,2,L)...(0,0,R)
-
-The 3 counts: illegal count, repeat count, total count for each solution.
+maintainer: Cunzhi Ren <renc@uw.edu>
+HomeWork1 of EE562 Artificial Intelligence for Engineers:
+Missionary-Cannibal Problem (with 3 missionaries and 3 cannibals)
+with a RECURSIVE DEPTH-FIRST SEARCH
 """
 
 
@@ -21,7 +14,7 @@ class State:
 
     def __init__(self, missionaries, cannibals, boat):
         """
-        This is an initial method
+        This is an initial method.
         :param missionaries: missionaries on the left bank
         :param cannibals: cannibals on the left bank
         :param boat: R means boat on the right bank, L means left
@@ -31,160 +24,151 @@ class State:
         self.missionaries_right = 3 - missionaries
         self.cannibals_right = 3 - cannibals
         self.boat = boat
-        self.parent = None
-        self.repeated_count = 0
-        self.state_format = '({0},{1},{2})'
-        self.repeat_count = 0
-
 
     def is_goal(self):
         """
-        This method checks if current state reaches goal
+        This method checks if current state reaches goal. If state reaches
+        goal then return true, else return false.
         :return: Boolean
         """
-        if self.missionaries is 0 and self.cannibals is 0 and self.boat is 'R':
-            return True
-        else:
-            return False
+        return not self.missionaries and not self.cannibals and self.boat is 'R'
 
     def is_legal(self):
         """
-        **********************repeat and illegal count*****************
-        :return:
+        This method checks if a state is legal. If not legal, illegal count
+        plus 1.
+        :return: Boolean
         """
-        if self.boat in {'L', 'R'} and 0 <= self.missionaries <= 3 and 0 <= self.cannibals <= 3 \
-                and (self.missionaries is 0 or self.missionaries >= self.cannibals) \
-                and (self.missionaries_right is 0 or self.missionaries_right >= self.cannibals_right):
+        global illegal_count
+        if (self.missionaries_right is 0 or self.missionaries_right >= self.cannibals_right) \
+                and self.boat in {'L', 'R'} and 0 <= self.missionaries <= 3 \
+                and 0 <= self.cannibals <= 3 \
+                and (self.missionaries is 0 or self.missionaries >= self.cannibals):
             return True
         else:
+            illegal_count += 1
             return False
 
-    def is_discovered(self, discovered_states):
+    def is_discovered(self, progress):
         """
         This method checks if state has been discovered
         before. If so, return False or else add it to
         discovered states set.
-        :param discovered_states: a set contains states discovered
+        :param progress: a list contains states used before
         :return: Boolean
         """
-        if (self.missionaries, self.cannibals, self.boat) in discovered_states:
-            self.repeat_count += 1
-            return False
-        else:
-            # discovered_states.add((self.missionaries, self.cannibals, self.boat))
+        global repeat_count
+        if (self.missionaries, self.cannibals, self.boat) in progress:
             return True
+        else:
+            repeat_count += 1
+            return False
 
     @staticmethod
     def successors(current_state):
         """
         This static method add legal successor states to list children
         :param current_state: current state
-        :return:children is a list that contains legal successor states
+        :return:children
         """
         children = []
-        if current_state.missionaries is 0 and current_state.cannibals is 0 and current_state.boat is 'R':
-            children = []
-        elif current_state.boat is 'L':
+        if current_state.boat is 'L':
             # 2 cannibals from left to right
             new_state = State(current_state.missionaries, current_state.cannibals - 2, 'R')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
             # 2 missionaries from left to right
             new_state = State(current_state.missionaries - 2, current_state.cannibals, 'R')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
             # 1 missionary and 1 cannibal from left to right
             new_state = State(current_state.missionaries - 1, current_state.cannibals - 1, 'R')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
             # 1 missionary from left to right
             new_state = State(current_state.missionaries - 1, current_state.cannibals, 'R')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
             # 1 cannibal from left to right
             new_state = State(current_state.missionaries, current_state.cannibals - 1, 'R')
-            State.operate_legal_state(new_state, current_state, children)
-        elif current_state.boat is 'R':
+            State.operate_legal_state(new_state, children)
+        if current_state.boat is 'R':
             # 2 cannibals from right to left
             new_state = State(current_state.missionaries, current_state.cannibals + 2, 'L')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
             # 2 missionaries from right to left
             new_state = State(current_state.missionaries + 2, current_state.cannibals, 'L')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
             # 1 missionary and 1 cannibal from right to left
             new_state = State(current_state.missionaries + 1, current_state.cannibals + 1, 'L')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
             # 1 missionary from right to left
             new_state = State(current_state.missionaries + 1, current_state.cannibals, 'L')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
             # 1 cannibal from right to left
             new_state = State(current_state.missionaries, current_state.cannibals + 1, 'L')
-            State.operate_legal_state(new_state, current_state, children)
+            State.operate_legal_state(new_state, children)
         return children
 
     @staticmethod
-    def operate_legal_state(new_state, current_state, children):
+    def operate_legal_state(new_state, children):
         """
         This static method checks if new state is legal then
         make current state parent of legal state and append it
         to list of children
-        :param new_state:
-        :param current_state:
-        :param children:
-        :return:
+        :param new_state: possible successor state of current state
+        :param children: a list contains contains legal successor states
+        :return: None
         """
         if new_state.is_legal():
-            new_state.parent = current_state
             children.append(new_state)
 
     @staticmethod
-    def DFS(current_state, discovered_states, progress):
-        discovered_states.add((current_state.missionaries,current_state.cannibals,current_state.boat))
-        # print(discovered_states)
-        for s in State.successors(current_state):
-            if s.is_discovered(discovered_states):
-                s.parent = current_state
-                progress.append(current_state)
-                current_state = s
-                print((s.parent.missionaries, s.parent.cannibals, s.parent.boat))
-                if s.is_goal():
-                    # print('~~~~~~~~~~~~~~finish~~~~~~~~~~~~~~')
-                    for i in range(4):
-                        State(0,0,'R').parent = s
-                        # discovered_states = set()
-                        # discovered_states.add((3,1,"R"))
-                        # State.DFS(State(3, 3, 'L'), discovered_states, progress)
-                else:
-                    State.DFS(current_state, discovered_states, progress)
+    def dfs(current_state, progress):
+        """
+        This static method implements Depth First Search recursively
+        generate progress from initial state to goal state
+        and keeps all the progresses in solutions.
+        :param current_state: current state implemented DFS
+        :param progress: the progress from initial state to goal
+        :return: None
+        """
+        if current_state.is_goal():
+            progress.append((current_state.missionaries, current_state.cannibals, current_state.boat))
+            global total_count
+            total_count += 1
+            solutions.append(progress[:])
+            progress.remove((current_state.missionaries, current_state.cannibals, current_state.boat))
+            return
+        if current_state.is_discovered(progress):
+            return
+        children = State.successors(current_state)
+        progress.append((current_state.missionaries, current_state.cannibals, current_state.boat))
+        total_count += 1
+        for child in children:
+            State.dfs(child, progress)
+        progress.remove((current_state.missionaries, current_state.cannibals, current_state.boat))
 
     @staticmethod
     def solution():
-        discovered_states = set()
+        """
+        This static method sets some initial values for problem,
+        solves Missionaries and Cannibals problem and prints solutions
+        with total, illegal, repeat counts.
+        :return: None
+        """
+        global repeat_count, illegal_count, total_count, solutions
+        repeat_count = 1
+        total_count = 0
+        illegal_count = 0
+        relunctant_illegal = 40
+        solutions = []
         initial_state = State(3, 3, 'L')
         progress = []
-        State.DFS(initial_state, discovered_states, progress)
-        for i in range(len(progress)):
-            print("parent-------{0}".format((progress[11].parent.missionaries, progress[11].parent.cannibals, progress[11].parent.boat)))
-        # while s.par
-        # return s
+        State.dfs(initial_state, progress)
+        for index, state in enumerate(solutions):
+            print("Solution{0}:".format(index + 1))
+            for state in solutions[index]:
+                print("{0}".format(state))
+        print("totals {0} illegals {1} repeats {2}".format(total_count,
+                                                           illegal_count - relunctant_illegal, repeat_count))
+
 
 if __name__ == '__main__':
-    # print(State.successors(State(0,0,'R')))
-    s = State.solution()
-
-    # s = State(3, 3, 'L')
-    # s = s.successors(s)
-    #
-    # for n in s:
-    #     print((n.missionaries, n.cannibals, n.boat))
-
-    # e = set()
-    # s0 = State(3, 3, 'L')
-    # repeated_count = 0
-    # s0.is_discovered(e)
-    # print(e)
-    # print("_______")
-    # # print(s.parent() == s)
-    # # print(s.is_goal())
-    # for n in s.successors(s):
-    #     n.is_discovered(e)
-    # print(repeated_count)
-    # print(e)
-    # else:
-    #     print("m:{0}, c:{1}, b:{2}, pm:{3},pc:{4},pb:{5}".format(n.missionaries, n.cannibals, n.boat, n.parent.missionaries, n.parent.cannibals, n.parent.boat))
+    State.solution()
